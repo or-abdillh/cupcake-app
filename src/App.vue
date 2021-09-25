f48803bfbb17784de8d92b39b1afe82589a25480
or-abdillh <hans.abdillh05@gmail.com>
2021-09-25 18:10:41 +0800

    Hide app if user use browser width more bigger than 464px

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
-   <component v-on:change-tab-to-details="currentTab = Details" v-on:change-tab-to-home="currentTab = Home" :is="currentTab" ></component>
+   <Alert v-if="!isMobile"></Alert>
+   <component v-if="isMobile" v-on:change-tab-to-details="currentTab = Details" v-on:change-tab-to-home="currentTab = Home" :is="currentTab" ></component>
 </template>
 
 <script setup>
    
    import Home from './views/Home.vue'
    import Details from './views/Details.vue'
-   import { ref } from 'vue'
+   import Alert from './components/Alert.vue'
+   import { ref, computed, onMounted, watch } from 'vue'
    
    const currentTab = ref(Home)
    const change = tab => {
       if ( tab === 'home' ) currentTab.value = Home
       else currentTab.value = Details
    }
+   
+   //Get the current viewport width
+   const viewPort = ref(window.innerWidth)
+   
+   //Hidden root if viewPort more bigger than 464
+   const isMobile = ref(false)
+   
+   //Checking viewport at onMounted
+   onMounted(() => {
+      if ( viewPort.value > 464 ) isMobile.value = false
+      else isMobile.value = true
+   })
+   
+   //Checking viewPort at resize browser
+   watch(viewPort, value => {
+      if ( value > 464 ) isMobile.value = false
+      else isMobile.value = true
+   })
    
 </script>
---
+++ src/components/Alert.vue
@@ -0,0 +1,18 @@
+<template>
+   <section class="alert">
+      <p class="font-medium text-2xl text-gray-400" >Sorry</p>
+      <h1 class="text-3xl text-gray-700 font-semibold">
+         This application does not support the screen width of your device
+         <br />
+         You can resize the width of your browser on the mobile display
+      </h1>
+   </section>
+</template>
+
+<style>
+   
+   .alert {
+      @apply w-8/12 bg-gray-100 shadow-xl rounded-xl px-12 py-10 mt-24 mx-auto;
+   }
+   
+</style>
