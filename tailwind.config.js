f02713db21353daa7a1142756f54e2c3551a825a
or-abdillh <hans.abdillh05@gmail.com>
2021-09-25 22:23:04 +0800

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,11 @@
+// tailwind.config.js
 module.exports = {
-  purge: ["./index.html', './src/**/*.{vue,js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-     minWidth: {
-       '0': '0',
-       '1/4': '25%',
-       '1/2': '50%',
-       '3/4': '75%',
-       'full': '100%',
-      },
-    extend: {},
-  },
-  variants: {
-    extend: {
-       textColor: ['active']
-    },
-  },
+  purge: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx',
+  ],
+  theme: {},
+  variants: {},
   plugins: [],
 }
